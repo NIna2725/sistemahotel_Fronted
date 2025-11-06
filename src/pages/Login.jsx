eab3ad1674a02e71fe9bb490e40fbe9cf149636b
Nina2527 <virxx3173@gmail.com.com>
2025-11-05 23:22:40 -0500

    first commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <h1 className="bg-amber-50">
+    Hello world!
+    </h1>
+    // <div>Login</div>
   )
 }
 
