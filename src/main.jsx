ad4f024804dc6a68fb799b7651e9620e9b97eedd
Nina2527 <virxx3173@gmail.com.com>
2025-11-05 23:44:44 -0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './routes/AppRouter.jsx'
+import { ChakraProvider } from '@chakra-ui/react'   
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
-    {/* <App /> */}
+    <ChakraProvider>
+{   /* <App /> */}
     <AppRouter/>
+    </ChakraProvider>
   </StrictMode>
   </BrowserRouter>
 )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
-
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from '@chakra-ui/react'
 const Login = () => {
   return (
-    <h1 className="bg-amber-50">
-    Hello world!
-    </h1>
+    <div className="bg-amber-50">
+    <Accordion>
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as='span' flex='1' textAlign='left'>
+          Section 1 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as='span' flex='1' textAlign='left'>
+          Section 2 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
+    </div>
     // <div>Login</div>
   )
 }
